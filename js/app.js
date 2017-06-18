6d47f9ded6a527e41c663ecff02391eeeacaf621
thechanchanman <anastasiospaliouras@gmail.com>
2017-06-18 18:29:00 +0300

    adding styles

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ var growler = new Vue({
     el: '#growler',
     data: {
         appName: 'Growler',
-        appLogo: 'img/logo.png'
+        appLogo: 'img/logo.png',
+        appStyle: {
+            color: '#FF6A00',
+            fontFamily: 'Verdana',
+            margin: 0
+        }
     }
 });
